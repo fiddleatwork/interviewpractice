32cef489c229fe2b7504fd37dddc20b42bd5cd76
Michael Mattox <michael.mattox@gmail.com>
2021-12-27 21:55:01 +0100

    optimal solution

--- src/test/kotlin/coding/leetcode/Leet011ContainerMostWater.kt
+++ src/test/kotlin/coding/leetcode/Leet011ContainerMostWater.kt
@@ -2,58 +2,27 @@ package coding.leetcode
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.lang.Math.pow
-import kotlin.math.abs
-import kotlin.math.pow
 
 
 class Leet011ContainerMostWater {
 
     @Test
     fun `example 1`() {
-        assertThat(listOf(1, 8, 6, 2, 5, 4, 8, 3, 7).mostWater()).isEqualTo(49)
+        assertThat(maxArea(intArrayOf(1, 8, 6, 2, 5, 4, 8, 3, 7))).isEqualTo(49)
     }
 
     @Test
     fun `example 2`() {
-        assertThat(listOf(1, 1).mostWater()).isEqualTo(1)
+        assertThat(maxArea(intArrayOf(1, 1))).isEqualTo(1)
     }
 
-    @Test
-    fun `big test`() {
-        assertThat(
-            listOf(
-                581,
-                3745,
-                6054,
-                3209,
-                3471,
-                7281,
-                2558,
-                2350,
-                5695,
-                7485,
-                4767,
-                2942,
-                9950,
-                8502,
-                9406,
-                6627
-            ).mostWater()
-        ).isEqualTo(78702)
-    }
-
-    fun maxArea(height: IntArray): Int {
-        return height.toList().mostWater()
-    }
-
-    private fun List<Int>.mostWater(): Int {
+    private fun maxArea(height: IntArray): Int {
         var left = 0
-        var right = size - 1
+        var right = height.size - 1
         var max = 0
         do {
-            max = Math.max((right - left) * Math.min(this[left], this[right]), max)
-            if (this[left] < this[right]) {
+            max = Math.max((right - left) * Math.min(height[left], height[right]), max)
+            if (height[left] < height[right]) {
                 left++
             } else {
                 right--
@@ -62,7 +31,7 @@ class Leet011ContainerMostWater {
         return max
     }
 
-    private fun List<Int>.mostWater2(): Int {
+    private fun List<Int>.mostWaterBruteForce(): Int {
         val results = mutableMapOf<Pair<Int, Int>, Int>()
         var maxResult = 0
         for (i in 0 until size) {
