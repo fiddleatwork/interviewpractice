fa36479b5e60f344e408e7ff5b98ba82dd3f99b5
Michael Mattox <michael.mattox@gmail.com>
2022-02-05 17:35:41 +0100

    final version with failing test

--- src/test/kotlin/coding/hackerrank/week1prep/day2/Day2MockTestFlippingTheMatrix.kt
+++ src/test/kotlin/coding/hackerrank/week1prep/day2/Day2MockTestFlippingTheMatrix.kt
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test
  * test page, so I can't see the details of the question.  I get a different result, but it looks ok to me
  * and since I can't compare it with their example test case, I have to pass on this one.
  */
-class Day2MockTest1 {
+class Day2MockTestFlippingTheMatrix {
 
     @Test
     fun `test case 1`() {
@@ -54,18 +54,20 @@ class Day2MockTest1 {
     }
 
     @Test
-    fun `should flip row`() {
+    fun `should reverse row`() {
         val array = arrayOf(
             arrayOf(10, 11, 12, 13),
             arrayOf(14, 15, 16, 17),
             arrayOf(18, 19, 20, 21),
             arrayOf(22, 23, 24, 25),
         )
-        array.flipRow(1)
+        print(array.toPrettyString())
+        array.reverseRow(1)
+        print(array.toPrettyString())
         assertThat(array).isDeepEqualTo(
             arrayOf(
                 arrayOf(10, 11, 12, 13),
-                arrayOf(16, 17, 14, 15),
+                arrayOf(17, 16, 15, 14),
                 arrayOf(18, 19, 20, 21),
                 arrayOf(22, 23, 24, 25),
             )
@@ -73,22 +75,22 @@ class Day2MockTest1 {
     }
 
     @Test
-    fun `should flip column`() {
+    fun `should reverse column`() {
         val array = arrayOf(
             arrayOf(10, 11, 12, 13),
             arrayOf(14, 15, 16, 17),
             arrayOf(18, 19, 20, 21),
             arrayOf(22, 23, 24, 25),
         )
-        println("Before:\n" + array.toPrettyString())
-        array.flipColumn(2)
-        println("After:\n" + array.toPrettyString())
+        print(array.toPrettyString())
+        array.reverseColumn(1)
+        print(array.toPrettyString())
         assertThat(array).isDeepEqualTo(
             arrayOf(
-                arrayOf(10, 11, 20, 13),
-                arrayOf(14, 15, 24, 17),
-                arrayOf(18, 19, 12, 21),
-                arrayOf(22, 23, 16, 25),
+                arrayOf(10, 23, 12, 13),
+                arrayOf(14, 19, 16, 17),
+                arrayOf(18, 15, 20, 21),
+                arrayOf(22, 11, 24, 25),
             )
         )
     }
@@ -120,52 +122,51 @@ class Day2MockTest1 {
     }
 
     private fun Array<Array<Int>>.toPrettyString(): String {
-        return map { a -> a.joinToString(",") { "$it" } }.joinToString("\n") { it }
+        return map { a -> a.joinToString(",") { "$it" } }.joinToString("\n") { it } + "\n"
     }
 
-    private fun Array<Array<Int>>.flipRow(r: Int) {
+    private fun Array<Array<Int>>.reverseRow(r: Int) {
         // 1 2 3 4
         // 5 6 7 8
         // 9 1 2 3
         // 4 5 6 7
-        for (i in 0 until size / 2) {
+        for (i in 0 until size/2) {
             val temp = this[r][i]
-            this[r][i] = this[r][i + size / 2]
-            this[r][i + size / 2] = temp
+            this[r][i] = this[r][size-1-i]
+            this[r][size-1-i] = temp
         }
     }
 
-    private fun Array<Array<Int>>.flipColumn(c: Int) {
+    private fun Array<Array<Int>>.reverseColumn(c: Int) {
         // 1 2 3 4
         // 5 6 7 8
         // 9 1 2 3
         // 4 5 6 7
         for (i in 0 until size / 2) {
             val temp = this[i][c]
-            this[i][c] = this[i + size / 2][c]
-            this[i + size / 2][c] = temp
+            this[i][c] = this[size-1-i][c]
+            this[size-1-i][c] = temp
         }
     }
 
-
     private fun flippingMatrix(matrix: Array<Array<Int>>): Int {
-        println("Starting matrix:\n" + matrix.toPrettyString())
+        //println("Starting matrix:\n" + matrix.toPrettyString())
         for (i in matrix.indices) {
             val (a, b) = matrix.twoSumsOfRow(i)
             if (b > a) {
-                matrix.flipRow(i)
-                println("\nFlipped row $i:\n" + matrix.toPrettyString())
+                matrix.reverseRow(i)
+                //println("\nFlipped row $i:\n" + matrix.toPrettyString())
             }
         }
         for (i in matrix.indices) {
             val (a, b) = matrix.twoSumsOfColumn(i)
             if (b > a) {
-                matrix.flipColumn(i)
-                println("\nFlipped column $i:\n" + matrix.toPrettyString())
+                matrix.reverseColumn(i)
+                //println("\nFlipped column $i:\n" + matrix.toPrettyString())
             }
         }
 
-        println("\nFinal:\n" + matrix.toPrettyString())
+        //println("\nFinal:\n" + matrix.toPrettyString())
         return matrix.sumUpperLeftQuadrant()
     }
 }
