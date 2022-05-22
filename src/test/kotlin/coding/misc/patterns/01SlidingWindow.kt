339092c19421260ed049b60caff93db02d29dc14
Michael Mattox <michael.mattox@gmail.com>
2022-05-22 07:03:21 +0200

    sliding window example

---
+++ src/test/kotlin/coding/misc/patterns/01SlidingWindow.kt
@@ -0,0 +1,32 @@
+package coding.misc.patterns
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class `01SlidingWindow` {
+
+    private fun List<Int>.averages(k: Int): List<Double> {
+        val averages = mutableListOf<Double>()
+        var sum = 0
+        indices.forEach{ i ->
+            //println("i=$i")
+            sum += get(i)
+            if(i >= k-1) {
+                averages.add(sum.toDouble()/k)
+                sum -= get(i-k+1)
+            }
+        }
+        return averages
+    }
+
+    @Test
+    fun example1() {
+        assertThat(
+            listOf(1, 3, 2, 6, -1, 4, 1, 8, 2).averages(5)
+        )    .isEqualTo(
+            listOf(2.2, 2.8, 2.4, 3.6, 2.8)
+        )
+    }
+
+}
+
