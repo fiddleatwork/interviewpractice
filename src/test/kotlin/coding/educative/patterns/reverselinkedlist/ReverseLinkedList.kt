2c060dba1560ddbb1d7f5dd4c9ab86e69a5880ef
Michael Mattox <michael.mattox@gmail.com>
2022-05-22 11:18:50 +0200

    needs optimization

---
+++ src/test/kotlin/coding/educative/patterns/reverselinkedlist/ReverseLinkedList.kt
@@ -0,0 +1,33 @@
+package coding.educative.patterns.reverselinkedlist
+
+import coding.educative.patterns.fastslowpointers.ListNode
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ReverseLinkedList {
+
+    fun ListNode.reversed(): ListNode? {
+        if(this == null) {
+            return null
+        }
+        var previous: ListNode? = null
+        var current: ListNode? = this
+        while(current?.next != null) {
+            val next = current.next
+            current.next = previous
+            previous = current
+            current = next
+        }
+        current?.next = previous
+        return current
+    }
+
+    @Test
+    fun example1() {
+        assertThat(
+            ListNode(2, ListNode(4, ListNode(6, ListNode(8, ListNode(10))))).reversed()
+        ).isEqualTo(
+            ListNode(10, ListNode(8, ListNode(6, ListNode(4, ListNode(2)))))
+        )
+    }
+}
