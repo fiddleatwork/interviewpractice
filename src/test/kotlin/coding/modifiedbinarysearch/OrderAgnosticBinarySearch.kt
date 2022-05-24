12ed0675153ef0ff7627dc0c5af0fa6d944f4e40
Michael Mattox <michael.mattox@gmail.com>
2022-05-24 22:38:26 +0200

    support descending sort

--- src/test/kotlin/coding/modifiedbinarysearch/OrderAgnosticBinarySearch.kt
+++ src/test/kotlin/coding/modifiedbinarysearch/OrderAgnosticBinarySearch.kt
@@ -9,6 +9,16 @@ class OrderAgnosticBinarySearch {
         if (isEmpty()) {
             return -1
         }
+        return when (first().compareTo(last())) {
+            1 -> size - 1 - reversed().searchIndexOf(k)
+            else -> searchIndexOfHelper(k)
+        }
+    }
+
+    private fun List<Int>.searchIndexOfHelper(k: Int): Int {
+        if (isEmpty()) {
+            return -1
+        }
         val start = size / 2
         return when {
             k == get(start) -> return start
@@ -32,5 +42,19 @@ class OrderAgnosticBinarySearch {
         assertThat(list.searchIndexOf(0)).isEqualTo(-1)
         assertThat(list.searchIndexOf(1)).isEqualTo(0)
         assertThat(list.searchIndexOf(100)).isEqualTo(99)
+    }
+
+    @Test
+    fun `handle duplicates`() {
+        assertThat(
+            listOf(1, 1, 2, 3, 3, 4, 5, 6, 7).searchIndexOf(2)
+        ).isEqualTo(2)
+    }
+
+    @Test
+    fun `handle sorted descending`() {
+        assertThat(
+            listOf(7, 6, 5, 4, 3, 2, 1).searchIndexOf(5)
+        ).isEqualTo(2)
     }
 }
