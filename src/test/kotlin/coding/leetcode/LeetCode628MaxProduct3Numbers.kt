153098e7cf739ecfdc855acbce767535f9e9a9e4
Michael Mattox <michael.mattox@gmail.com>
2022-05-29 11:53:16 +0200

    first attempt using heaps

---
+++ src/test/kotlin/coding/leetcode/LeetCode628MaxProduct3Numbers.kt
@@ -0,0 +1,68 @@
+package coding.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.math.abs
+import kotlin.math.max
+
+class LeetCode628MaxProduct3Numbers {
+
+    fun maximumProduct(nums: IntArray): Int {
+        val minHeap = PriorityQueue<Int> { a, b -> a - b }
+        val maxHeap = PriorityQueue<Int> { a, b -> b - a }
+        (0 until 3).forEach { i ->
+            minHeap.add(nums[i])
+        }
+        (0 until 2).forEach { i ->
+            maxHeap.add(nums[i])
+        }
+        (3 until nums.size).forEach { i ->
+            if (nums[i] > minHeap.peek()) {
+                minHeap.poll()
+                minHeap.add(nums[i])
+            }
+        }
+        (2 until nums.size).forEach { i ->
+            if (nums[i] < maxHeap.peek()) {
+                maxHeap.poll()
+                maxHeap.add(nums[i])
+            }
+        }
+        val secondSmallest = maxHeap.poll()
+        val smallest = maxHeap.poll()
+        val thirdLargest = minHeap.poll()
+        val secondLargest = minHeap.poll()
+        val largest = minHeap.poll()
+
+        return max(largest * secondLargest * thirdLargest, smallest * secondSmallest * largest)
+    }
+
+    @Test
+    fun example1() {
+        assertThat(
+            maximumProduct(intArrayOf(1, 2, 3))
+        ).isEqualTo(6)
+    }
+
+    @Test
+    fun example2() {
+        assertThat(
+            maximumProduct(intArrayOf(1, 2, 3, 4))
+        ).isEqualTo(24)
+    }
+
+    @Test
+    fun example3() {
+        assertThat(
+            maximumProduct(intArrayOf(-1, -2, -3))
+        ).isEqualTo(-6)
+    }
+
+    @Test
+    fun exampleTwoNegatives() {
+        assertThat(
+            maximumProduct(intArrayOf(-100, -98, -1, 2, 3, 4))
+        ).isEqualTo(39200)
+    }
+}
