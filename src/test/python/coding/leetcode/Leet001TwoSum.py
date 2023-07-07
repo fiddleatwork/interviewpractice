6d1fc25506762acc482bba92ae8b28d49cc871cd
Michael Mattox <michael.mattox@gmail.com>
2023-07-07 12:26:33 +0200

    add solution

--- src/test/python/coding/leetcode/Leet001TwoSum.py
+++ src/test/python/coding/leetcode/Leet001TwoSum.py
@@ -1,4 +1,5 @@
 import unittest
+from types import MappingProxyType
 
 from assertpy import assert_that
 
@@ -13,12 +14,19 @@ class MyTestCase(unittest.TestCase):
                     return i, j
         return ()
 
-
+    @staticmethod
+    def two_sum(nums: int(), target: int) -> int():
+        lookup = MappingProxyType({target - nums[i]: i for i, _ in enumerate(nums)})
+        for i, n in enumerate(nums):
+            m = lookup.get(n, None)
+            if m is not None and m != i:
+                return i, m
+        return ()
 
     @staticmethod
     def test_case_1():
         assert_that(
-            MyTestCase.two_sum_brute((2, 7, 11, 15), 9)
+            MyTestCase.two_sum((2, 7, 11, 15), 9)
         ).is_equal_to((0, 1))  # add assertion here
 
 
