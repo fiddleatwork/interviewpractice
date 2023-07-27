04b33844f0c17632809aedf9aa5905d0bf7f2573
Michael Mattox <michael.mattox@gmail.com>
2023-07-27 10:31:58 +0200

    replace functions with dictionary

--- src/test/python/coding/leetcode/leet013_roman_to_number.py
+++ src/test/python/coding/leetcode/leet013_roman_to_number.py
@@ -4,53 +4,54 @@ from types import MappingProxyType
 from assertpy import assert_that
 from typing import Tuple
 
+
 @dataclass
 class Increment:
     amount: int
     counter: int
 
+
 @staticmethod
 def roman_to_int(s: str) -> int:
     
-    def calculate_increment_single(s: str, i: int) -> Increment:
-        # print(f"s = {s}")
-        match s:
-            case "I" : return Increment(1, 1)
-            case "V" : return Increment(5, 1)
-            case "X" : return Increment(10, 1)
-            case "L" : return Increment(50, 1)
-            case "C" : return Increment(100, 1)
-            case "D" : return Increment(500, 1)
-            case "M" : return Increment(1000, 1)
-            case _ :  raise Exception(f"Unrecognized symbol {s}")
+    lookup_doubles = {
+        "IV": Increment(4, 2),
+        "IX": Increment(9, 2),
+        "XL": Increment(40, 2),
+        "XC": Increment(90, 2),
+        "CD": Increment(400, 2),
+        "CM": Increment(900, 2),
+    }
 
-    def calculate_increment(s: str, i: int) -> Increment:
-        # print(f"match_double: s = {s} sub2={sub2} i={i} s[i:i]={s[i:i+1]}")
-        match s:
-            case "IV" : return Increment(4, 2)
-            case "IX" : return Increment(9, 2)
-            case "XL" : return Increment(40, 2)
-            case "XC" : return Increment(90, 2)
-            case "CD" : return Increment(400, 2)
-            case "CM" : return Increment(900, 2)
-            case _ : return calculate_increment_single(s[0:1], i)
-               
+    lookup_singles = {
+        "I": Increment(1, 1),
+        "V": Increment(5, 1),
+        "X": Increment(10, 1),
+        "L": Increment(50, 1),
+        "C": Increment(100, 1),
+        "D": Increment(500, 1),
+        "M": Increment(1000, 1),
+    }
+
     i = 0
-    result = 0                                    
+    result = 0
     while i < len(s):
-        if i < len(s)-1:
-            sub2 = s[i:i+2]
+        
+        if i < len(s) - 1:
+            sub2 = s[i : i + 2]
         else:
-            sub2 = s[i:i+1]
-        increment = calculate_increment(sub2, i)
+            sub2 = s[i : i + 1]
+            
+        increment = lookup_doubles.get(sub2)
+        if increment is None:
+            increment = lookup_singles[sub2[0:1]]
         result += increment.amount
-        i+= increment.counter
+        i += increment.counter
 
     return result
 
 
 class Leet013RomanToNumber(unittest.TestCase):
-
     @staticmethod
     def test_case_1():
         assert_that(roman_to_int("III")).is_equal_to(3)
