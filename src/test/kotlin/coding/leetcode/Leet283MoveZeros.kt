2630f57fb682cfd498b45ed73fed176ef04d5c81
Michael Mattox <michaelpub@gmail.com>
2024-01-23 07:37:42 -0500

    more solutions

--- src/test/kotlin/coding/leetcode/Leet283MoveZeros.kt
+++ src/test/kotlin/coding/leetcode/Leet283MoveZeros.kt
@@ -6,35 +6,32 @@ import kotlin.math.pow
 
 class Leet283MoveZeros {
 
-    fun moveZeroes(nums: IntArray) {
-        if(nums.size < 2) {
+    private fun IntArray.nextNonZeroIndex(i: Int): Int =
+        toList().subList(i + 1, size).indexOfFirst { n -> n != 0 } + (i+1)
+
+    private fun moveZeroes(nums: IntArray) {
+        if (nums.size < 2) {
             return
         }
-        var swapped = false
-        (0..<nums.size-1).forEach { i ->
-            if(nums[i] == 0 && nums[i+1] != 0) {
+        (0..<nums.size - 1).forEach { i ->
+            if (nums[i] == 0) {
+                val indexToSwap = nums.nextNonZeroIndex(i)
                 val temp = nums[i]
-                nums[i] = nums[i+1]
-                nums[i+1] = temp
-                swapped = true
+                nums[i] = nums[indexToSwap]
+                nums[indexToSwap] = temp
             }
-        }
-        if(swapped) {
-            moveZeroes(nums)
         }
     }
 
     // 0,1,0,3,12
     // 1,0,0,3,12
     // 1,0,3,0,12
-    // 1,3,0,0,12
-    // 1,3,0,12,0
-    // 1,3,12,0,0
+    // 1,0,3,12,0
     @Test
     fun example1() {
         val nums = intArrayOf(0, 1, 0, 3, 12)
         moveZeroes(nums)
-        assertThat(nums).isEqualTo(intArrayOf(1,3,12,0,0))
+        assertThat(nums).isEqualTo(intArrayOf(1, 3, 12, 0, 0))
     }
 
     @Test
@@ -46,9 +43,15 @@ class Leet283MoveZeros {
 
     @Test
     fun test01() {
-        val nums = intArrayOf(0,1)
+        val nums = intArrayOf(0, 1)
         moveZeroes(nums)
-        assertThat(nums).isEqualTo(intArrayOf(1,0))
+        assertThat(nums).isEqualTo(intArrayOf(1, 0))
+    }
+
+    @Test
+    fun `should get index of next non-zero element`() {
+        assertThat(intArrayOf(1, 0, 0, 3, 12).nextNonZeroIndex(0)).isEqualTo(3)
+        assertThat(intArrayOf(1, 2, 3, 0, 12).nextNonZeroIndex(2)).isEqualTo(4)
     }
 
 
