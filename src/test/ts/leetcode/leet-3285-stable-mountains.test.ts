ac6a818c628a56d6856a1ae84dd3cfa5cde9b13a
Michael Mattox <michaelpub@gmail.com>
2024-11-05 11:26:51 -0500

    more solutions

---
+++ src/test/ts/leetcode/leet-3285-stable-mountains.test.ts
@@ -0,0 +1,35 @@
+
+function stableMountains(height: number[], threshold: number): number[] {
+    var stableIndices: number[] = []
+    height.forEach((h,i) => {
+        if(i > 0 && height[i-1] > threshold) {
+            stableIndices.push(i)
+        }
+    })
+    return stableIndices
+};
+
+
+describe('findStableMountains', () => {
+
+    it('should return [3, 4] for height = [1, 2, 3, 4, 5] and threshold = 2', () => {
+        const height = [1, 2, 3, 4, 5];
+        const threshold = 2;
+        const result = stableMountains(height, threshold);
+        expect(result).toEqual(expect.arrayContaining([3, 4]));
+    });
+
+    it('should return [1, 3] for height = [10, 1, 10, 1, 10] and threshold = 3', () => {
+        const height = [10, 1, 10, 1, 10];
+        const threshold = 3;
+        const result = stableMountains(height, threshold);
+        expect(result).toEqual(expect.arrayContaining([1, 3]));
+    });
+
+    it('should return [] for height = [10, 1, 10, 1, 10] and threshold = 10', () => {
+        const height = [10, 1, 10, 1, 10];
+        const threshold = 10;
+        const result = stableMountains(height, threshold);
+        expect(result).toEqual([]);
+    });
+});
