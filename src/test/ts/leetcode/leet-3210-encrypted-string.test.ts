f2964cdf00ab561b297d9c88da68790e9d1d90e4
Michael Mattox <michaelpub@gmail.com>
2024-11-06 06:41:47 -0500

    more solutions

--- src/test/ts/leetcode/leet-3210-encrypted-string.test.ts
+++ src/test/ts/leetcode/leet-3210-encrypted-string.test.ts
@@ -1,35 +1,30 @@
 
 function getEncryptedString(s: string, k: number): string {
-    var chars = Array.from(s).map((_,i) => {
-        return wrappingGetChar(s, i+k)
-    })
-    return chars.join("")
+    return Array.from(s)
+        .map((_, i) => wrappingGetChar(s, i + k))
+        .join("")
 };
 
 function wrappingGetChar(s: string, n: number): string {
-    var index = n
-    while(index >= s.length) {
-        index -= s.length
-    }
-    return s.charAt(index)
+    return s.charAt(n % s.length)
 }
 
 describe("encrypted string", () => {
     it("example1", () => {
-        expect(getEncryptedString("dart",3)).toEqual("tdar")
+        expect(getEncryptedString("dart", 3)).toEqual("tdar")
     })
     it("example2", () => {
-        expect(getEncryptedString("aaa",1)).toEqual("aaa")
+        expect(getEncryptedString("aaa", 1)).toEqual("aaa")
     })
 
     it("should get character after wrapping around", () => {
-        expect(wrappingGetChar("abc",0)).toEqual("a")
-        expect(wrappingGetChar("abc",1)).toEqual("b")
-        expect(wrappingGetChar("abc",2)).toEqual("c")
-        expect(wrappingGetChar("abc",3)).toEqual("a")
-        expect(wrappingGetChar("abc",4)).toEqual("b")
-        expect(wrappingGetChar("abc",5)).toEqual("c")
-        expect(wrappingGetChar("abc",6)).toEqual("a")
+        expect(wrappingGetChar("abc", 0)).toEqual("a")
+        expect(wrappingGetChar("abc", 1)).toEqual("b")
+        expect(wrappingGetChar("abc", 2)).toEqual("c")
+        expect(wrappingGetChar("abc", 3)).toEqual("a")
+        expect(wrappingGetChar("abc", 4)).toEqual("b")
+        expect(wrappingGetChar("abc", 5)).toEqual("c")
+        expect(wrappingGetChar("abc", 6)).toEqual("a")
     })
 
 })
