3106e8b6be190b301d355c181fa4cda0f667e495
Michael Mattox <michaelpub@gmail.com>
2024-12-03 09:59:44 -0500

    more solutions

--- src/test/kotlin/coding/leetcode/Leet017LetterCombinationsPhoneNumber.kt
+++ src/test/kotlin/coding/leetcode/Leet017LetterCombinationsPhoneNumber.kt
@@ -11,29 +11,20 @@ class Leet017LetterCombinationsPhoneNumberTest {
         }
 
         val letters = mapOf(
-            "2" to listOf("a", "b", "c"),
-            "3" to listOf("d", "e", "f"),
-            "4" to listOf("g", "h", "i"),
-            "5" to listOf("j", "k", "l"),
-            "6" to listOf("m", "n", "o"),
-            "7" to listOf("p", "q", "r", "s"),
-            "8" to listOf("t", "u", "v"),
-            "9" to listOf("w", "x", "y", "z"),
+            '2' to listOf("a", "b", "c"),
+            '3' to listOf("d", "e", "f"),
+            '4' to listOf("g", "h", "i"),
+            '5' to listOf("j", "k", "l"),
+            '6' to listOf("m", "n", "o"),
+            '7' to listOf("p", "q", "r", "s"),
+            '8' to listOf("t", "u", "v"),
+            '9' to listOf("w", "x", "y", "z"),
         )
-        val chars = digits.toCharArray().toList().drop(1)
-        var result = letters[digits.first().toString()]!!
-        // 23
-        // result = a,b,c
-        // char = 3
-        // d,e,f
-        chars.forEach { c ->
-            val newResult = result.flatMap {rc ->
-                // rc a,b,c
-                letters[c.toString()]!!.map {rc + it}
+        return digits.fold(listOf("")) {acc, c ->
+            acc.flatMap {prefix ->
+                letters[c]!!.map {prefix + it}
             }
-            result = newResult
         }
-        return result
     }
 
 
